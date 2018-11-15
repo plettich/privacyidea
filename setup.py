238d6b5e31323dd02ac33001503722be9767b6c5
Paul Lettich <paul.lettich@netknights.it>
2018-11-15 16:43:58 +0100

    Testing travis to check pylint

--- setup.py
+++ setup.py
@@ -68,7 +68,8 @@ install_requires = ["Flask>=0.10.1",
                     "python-gnupg>=0.3.8",
                     "defusedxml>=0.4.1",
                     "flask-babel>=0.9",
-                    "croniter>=0.3.8"
+                    "croniter>=0.3.8",
+                    "six>=1.11.0"
                     ]
 
 # For python 2.6 we need additional dependency importlib
